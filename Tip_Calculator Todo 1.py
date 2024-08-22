ae6b5e3d8ee784cc21d798c62609d4b690f524a8
RavensFord <selormfarmer1@gmail.com>
2024-08-22 05:09:52 +0000

    vs_code

---
+++ Tip_Calculator Todo 1.py
@@ -0,0 +1,13 @@
+food_charge = float(input("Enter the charge for the food: "))
+
+tip_amount = food_charge * 0.18
+
+sales_tax_amount = food_charge * 0.07
+
+total_amount = food_charge + tip_amount + sales_tax_amount
+
+print("Food Charge: ${food_charge amount}")
+print("Tip Amount (18%): ${tip_amount:")
+print(f"Sales Tax Amount (7%): ${sales_tax_amount:}")
+print(f"Total Amount: ${total_amount:}")
+
